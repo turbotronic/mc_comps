44451b9f883486a04ffff9e26410150ce1fdd64a
jboho <bohoj@mac.com>
2013-10-10 16:08:37 -0600

    New icons and phone tweaks

--- temps/header.php
+++ temps/header.php
@@ -12,11 +12,26 @@
 	<meta name="dcterms.dateCopyrighted" content="<?php echo date('Y'); ?>" />
 	<meta name="description" content="" />
 	<link rel="canonical" href="<?php echo BASE_URL; ?>" />
-	<!-- FACEBOOK META -->
-	<meta property="og:title" content="<?php echo THE_TITLE; ?>"/>
-    <meta property="og:type" content="website"/>
-    <meta property="og:url" content="<?php echo BASE_URL; ?>"/>
-    <meta property="og:image" content=""/>
+	
+	
+	<!-- ICONS -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/touch/apple-touch-icon-144x144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/touch/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/touch/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="img/touch/apple-touch-icon-57x57-precomposed.png">
+    <link rel="shortcut icon" href="img/touch/apple-touch-icon.png">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
+	
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileImage" content="img/touch/apple-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="#222222">
+
+	<!-- iOS apps -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Media Center">
+
+
 	<!-- MODERNIZER -->
 	<script src="<?php echo JS_DIR; ?>/lib/modernizr.js"></script>
 	<!-- STYLESHEETS -->
