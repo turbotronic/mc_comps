fc4b2989dea383df529923b69a9e2a326a8dab07
jboho <bohoj@mac.com>
2013-10-09 18:52:50 -0600

    Menu options added

--- temps/footer.php
+++ temps/footer.php
@@ -1,14 +1,31 @@
 	<footer id="site-footer">
 		<div class="container">
 			<p>Copyright Media Center</p>
-			
-			<button type="button" class="menu-toggle"><span class="glyphicon glyphicon-list"><span class="sr-only">Menu</span></span></button>
-			
+			<button type="button" href="#wheel3" class="menu-toggle ne wheel-button"><span class="glyphicon glyphicon-list"><span class="sr-only">Menu</span></span></button>
 			<button type="button" data-container=".filter-toggle" data-toggle="popover" data-placement="auto top" data-header="Filters" data-html="true" data-title="<h4>Filters</h4>" data-content='<?php include('temps/ui/filters.php');?>' class="filter-toggle pull-right"><span class="glyphicon glyphicon-filter"><span class="sr-only">Filter</span></span></button>
+
 				
 			<button type="button" data-container=".settings-toggle" data-toggle="popover" data-placement="top" data-header="Settings" data-html="true" data-title="<h4>Settings</h4>" data-content='<?php include('temps/ui/settings-menu.php');?>' class="settings-toggle pull-right"><span class="glyphicon glyphicon-cog"><span class="sr-only">Settings</span></span></button> 
 		</div>
 	</footer>
+	
+	
+	
+	
+	
+	<ul id="wheel3" data-angle="NE" class="wheel">
+        <li class="news"><a href="#home"><span class="glyphicon glyphicon-floppy-open"></span><span class="menu-title">News</span></a></li>
+        <li class="sports"><a  href="#home"><span class="glyphicon glyphicon-glass"></span><span class="menu-title">Sports</span></a></li>
+        <li class="entertainment"><a  href="#home"><span class="glyphicon glyphicon-hd-video"></span><span class="menu-title ">Entertainment</span></a></li>
+        <li class="images"><a href="#home"><span class="glyphicon glyphicon-hdd"></span><span class="menu-title">Images of the Day</span></a></li>
+        <li class="projects"><a href="#home"><span class="glyphicon glyphicon-home"></span><span class="menu-title ">Special projects</span></a></li>
+        <li class="video"><a href="#home"><span class="glyphicon glyphicon-hd-video"></span><span class="menu-title">Videos</span></a></li>
+      </ul>
+
+
+
+
+
 	<?php include('temps/ui/signin.php'); ?>
 
 	<!-- JAVASCRIPTS -->
@@ -22,6 +39,7 @@
 	<![endif]-->
 	<script src="<?php echo JS_DIR; ?>/plugins/bootstrap.min.js"></script>
 	<script src="<?php echo JS_DIR; ?>/plugins/packery.min.js"></script>
+	<script src="<?php echo JS_DIR; ?>/plugins/wheelmenu.js"></script>
 	
 	<script type="text/javascript">
 	var container = document.querySelector('#container'),
@@ -33,6 +51,16 @@
 		pckry = new Packery(container, pckryOptions);
 	
 	$(function() {
+		
+		// active wheel menu
+		
+		// $('.wheel-button').wheelmenu({
+		// 	      trigger: "click",
+		// 	      animation: "fly",
+		// 	      angle: "NW"
+		// 	    });
+	
+		
 		
 		// toggle actives
 		$('.settings-toggle').click(function(){
@@ -47,7 +75,10 @@
 				$('.settings-toggle').popover('hide').removeClass('active');
 			}
 		});
-		$('.menu-toggle').click(function(){$(this).toggleClass('active')});
+		$('.menu-toggle').click(function(){
+			$(this).toggleClass('active');
+			$('.wheel').toggleClass('active');
+			});
 		
 		// reset everything on resize
 		$(window).resize(function(){
