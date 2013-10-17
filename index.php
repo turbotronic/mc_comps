3553bd1125a03071faf9b6ed3519c079423939a2
jboho <bohoj@mac.com>
2013-10-17 13:30:55 -0600

    Change time display

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 			<h3><a href="">Comedy Bang! Bang!” returns for the second half of season 2 on Oct. 18 on IFC</a></h3>
 			<div class="excerpt"><p>If you liked “Mr Show,” the HBO show and podcast, you probably like “Comedy Bang! Bang,” likewise from Scott Aukerman (co-creator/director/producer of “Between Two Ferns with Zach Galifianakis”). The rough-edged comedy-variety half-hour is a sendup of the late-night TV chat show format, drawing on well-known comedians and actors.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">Entertainment</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">Entertainment</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
@@ -30,7 +30,7 @@
 			<h3><a href="">San Jose Earthquakes defeat Colorado Rapids</a></h3>
 			<div class="excerpt"><p>Chris Wondolowski scored the lone goal for the San Jose Earthquakes against the Colorado Rapids in Santa Clara on Wednesday, Oct. 9.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">Sports</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">Sports</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
@@ -52,7 +52,7 @@
 			<h3><a href="">Pilgrims Arrive in Mecca for Hajj</a></h3>
 			<div class="excerpt"><p>More than two million Muslims have arrived in the holy city of Mecca for the annual hajj pilgrimage. The hajj, which is one of the world’s largest human assemblies, begins on October 13 amid concerns over the deadly MERS coronavirus.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">News</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">News</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
@@ -74,7 +74,7 @@
 			<h3><a href="">Archive Picture of the Day</a></h3>
 			<div class="excerpt"><p>This is a collection of pictures from The Denver Post’s digitized photographic archives, updated with a new image every day.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">News</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">News</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
@@ -96,7 +96,7 @@
 			<h3><a href="">Regis Dominates 5A Colorado State Golf Championships</a></h3>
 			<div class="excerpt"><p>The Regis Raiders captured their fourth-consecutive Class 5A championship in boys golf on Tuesday, October 1st at Murphy Creek in Aurora. Regis Jesuit finished at 7-under after the two-day tournament — outclassing second-place Ralston Valley by 23 strokes.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">Sports</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">Sports</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
@@ -118,7 +118,7 @@
 			<h3><a href="">National and International Mug Shots — The Rogues Gallery</a></h3>
 			<div class="excerpt"><p>Mug shots of criminals, movie stars, and sports stars from around the world. Not everyone who appears here has been convicted of a crime. Do not rely on this site to determine any person’s actual criminal record.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">Images of the day</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">Images of the day</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
@@ -140,7 +140,7 @@
 			<h3><a href="">Peru’s Ashaninka Indians Confront Modernity</a></h3>
 			<div class="excerpt"><p>Kimkibiri Baja, one of 350 Ashaninka communities, lies in the Apurimac river valley, the No. 1 coca-producing valley in the world, sharing the world’s top coca-growing valley with drug traffickers, rebels, illegal loggers and, now, an increased military presence.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">News</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">News</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
@@ -162,7 +162,7 @@
 			<h3><a href="">Montbello vs Standley Lake In Colorado Class 4A Football</a></h3>
 			<div class="excerpt"><p>tandley Lake High School defeated Montbello High School 31-21 at NAAC Stadium. Arvada, Colorado. October 3, 2013.</div></p>
 			<div class="meta">
-				<p><span class="category">In <a href="#">Sports</a></span><span class="timestamp">10/19/13 at 10:31 pm.</span></p>
+				<p><span class="category">In <a href="#">Sports</a></span><span class="timestamp">23m ago</span></p>
 				<div class="share-zipper">
 					<div class="drawer">
 						<ul class="share-tools">
