a2eec0884b030d552d5dda5faaf746ad3452d17d
jboho <bohoj@mac.com>
2014-01-16 14:32:45 -0700

    Update automation files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,86 +1,180 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict';
 
+  // Force use of Unix newlines
+  grunt.util.linefeed = '\n';
+
+  RegExp.quote = function (string) {
+    return string.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&')
+  }
+
+  var fs = require('fs')
+
+  // Project configuration.
   grunt.initConfig({
+
+    // Metadata.
     pkg: grunt.file.readJSON('package.json'),
+    banner: '/*!\n' +
+              ' * <%= pkg.name %> v<%= pkg.version %>\n' +
+              ' * Built using Strapit v<%= pkg.strapitVersion %> \n' + 
+              ' * Homepage: <%= pkg.homepage %>\n' +
+              ' * GitHub: <%= pkg.repository.url %>\n' +
+              ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+              ' */\n',
+
+    // Task configuration.
+
+    jshint: {
+      options: {
+        jshintrc: 'js/.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: ['js/*.js']
+      },
+      assets: {
+        src: ['js/<%= pkg.name %>.js']
+      }
+    },
+
+    csslint: {
+      options: {
+        csslintrc: 'less/.csslintrc'
+      },
+      src: [
+        'css/<%= pkg.name %>.css'
+      ]
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: false
+      },
+      scriptFiles: {
+        src: [
+
+        ],
+        dest: 'js/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      bootstrap: {
+        options: {
+          banner: '<%= banner %>\n',
+          report: 'min'
+        },
+        src: ['<%= concat.scriptFiles.dest %>'],
+        dest: 'js/<%= pkg.name %>.min.js'
+      },
+    },
 
     less: {
       compileCore: {
         options: {
           strictMath: true,
           sourceMap: true,
-          outputSourceFiles: true
+          outputSourceFiles: true,
+          sourceMapURL: 'css/<%= pkg.name %>.css.map',
+          sourceMapFilename: 'css/<%= pkg.name %>.css.map'
         },
         files: {
-          'style.css': 'less/style.less'
+          'css/<%= pkg.name %>.css': 'less/strapit.less'
         }
       },
       minify: {
         options: {
           cleancss: true,
-
+          report: 'min'
         },
         files: {
-          'style.min.css': 'style.css'
+          'css/<%= pkg.name %>.min.css': 'css/<%= pkg.name %>.css'
         }
       }
     },
 
-    concat: {
+    usebanner: {
       dist: {
-        src: [
-          'js/script.js'
-        ],
-        dest: 'assets/js/script.js'
+        options: {
+          position: 'top',
+          banner: '<%= banner %>'
+        },
+        files: {
+          src: [
+            'css/<%= pkg.name %>.css',
+            'css/<%= pkg.name %>.min.css'
+          ]
+        }
       }
     },
 
-    uglify: {
-      build: {
-        src: 'js/script.js',
-        dest: 'js/script.min.js'
+    csscomb: {
+      sort: {
+        options: {
+          config: 'less/.csscomb.json'
+        },
+        files: {
+          'css/<%= pkg.name %>.css': ['css/<%= pkg.name %>.css']
+        }
       }
     },
-
-    // connect: {
-    //   server: {
-    //     options: {
-    //       port: 8000,
-    //       base: ''
-    //     }
-    //   }
-    // },
 
     watch: {
       reloader: {
-        files: ['*.html', '*.php'],
+        files: ['*.html', '*.php', '/*/*.html', '/*/*.php'],
         options: {
           livereload: true
         }
       },
-      styles: {
-        // Which files to watch (all .less files recursively in the less directory)
-        files: ['less/style.less'],
-        tasks: ['less'],
+      src: {
+        files: '<%= jshint.src.src %>',
+        tasks: ['jshint:src'],
         options: {
           livereload: true
         }
       },
-      scripts: {
-        // Which files to watch (all .less files recursively in the less directory)
-        files: ['js/script.js'],
-        tasks: ['concat', 'uglify'],
+      less: {
+        files: ['less/*.less', 'less/*/*.less'],
+        tasks: ['less', 'csscomb', 'usebanner'],
         options: {
           livereload: true
         }
+      }
+    },
+
+    sed: {
+      versionNumber: {
+        pattern: (function () {
+          var old = grunt.option('oldver')
+          return old ? RegExp.quote(old) : old
+        })(),
+        replacement: grunt.option('newver'),
+        recursive: true
       }
     }
 
   });
 
-  require('load-grunt-tasks')(grunt);
 
-  // Default Task(s)
-  grunt.registerTask('default', ['watch']);
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+  // JS distribution task.
+  grunt.registerTask('dist-js', ['concat', 'uglify']);
+
+  // CSS distribution task.
+  grunt.registerTask('dist-css', ['less', 'csscomb', 'usebanner']);
+
+  // Default task.
+  grunt.registerTask('default', ['dist-css', 'dist-js']);
+
+  // Version numbering task.
+  // grunt change-version-number --oldver=A.B.C --newver=X.Y.Z
+  // This can be overzealous, so its changes should always be manually reviewed!
+  grunt.registerTask('change-version-number', ['sed']);
 
 };
