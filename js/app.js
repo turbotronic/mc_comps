35e6e7ff41f1a400a8f9ca6c0b654cec915b99a9
jboho <bohoj@mac.com>
2014-01-16 14:37:01 -0700

    Freaky-ness

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// On document load -- jQuery
+$(document).ready(function() {
+	
+	// VARS
+	var projectURL = ''; // base url for project for use in ajax apps
+
+	////////////////////
+	// AJAX FUNCTIONS //
+	////////////////////
+	
+	$.ajaxSetup({ cache: false });
+	
+	/////////////
+	// ON LOAD //
+	/////////////
+	
+	// place on load functions here
+
+});
