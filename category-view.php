b795954794d3d58ef00c957a769664e91c6d165d
jboho <bohoj@mac.com>
2014-01-24 10:12:02 -0700

    Adding category view comp

---
+++ category-view.php
@@ -0,0 +1,25 @@
+<?php require_once('inc/init.php'); // load in base objects ?>
+<?php $page_type = "category"; // define page ?>
+<?php require_once('temps/header.php'); // load in header ?>
+<div id="content" class="container category">
+	<div class="block-refer">
+		<a href=""><img src="img/test_img/img0.jpg" alt="" /></a>
+		<h3><a href="">Comedy Bang! Bang!” returns for the second half of season 2 on Oct. 18 on IFC</a></h3>
+		<div class="excerpt"><p>If you liked “Mr Show,” the HBO show and podcast, you probably like “Comedy Bang! Bang,” likewise from Scott Aukerman (co-creator/director/producer of “Between Two Ferns with Zach Galifianakis”). The rough-edged comedy-variety half-hour is a sendup of the late-night TV chat show format, drawing on well-known comedians and actors.</div></p>
+		<div class="meta">
+			<p><span class="category">In <a href="#">Entertainment</a></span><span class="timestamp">23m ago</span></p>
+			<div class="share-zipper">
+				<div class="drawer">
+					<ul class="share-tools">
+						<li><a class="fc-webicon facebook large" href=""><span class="sr-only">Facebook</span></a></li>
+						<li><a class="fc-webicon twitter large" href=""><span class="sr-only">Twitter</span></a></li>
+						<li><a class="fc-webicon googleplus large" href=""><span class="sr-only">Google Plus</span></a></li>
+						<li><a class="fc-webicon mail large" href=""><span class="sr-only">Email</span></a></li>
+					</ul> <!-- .share-tools -->
+				</div> <!-- .drawer -->
+			<button class="share-toggle"><span class="glyphicon glyphicon-share-alt"><span class="sr-only">Share</span></span></button>
+			</div> <!-- .share-zipper -->
+		</div> <!-- .meta -->
+	</div> <!-- .block-refer -->
+</div> <!-- #content -->
+<?php require_once('temps/footer.php'); // load in footer ?>
